d5abf7f2bfa0655df03a37b441b4fd5425fe0219
Cameron Hendrick <cameron@healthsystems.co.za>
2026-07-03 16:19:47 +0200

    Add realtime updates for mission messages

--- lib/realtime/commandCenterEvents.ts
+++ lib/realtime/commandCenterEvents.ts
@@ -11,6 +11,7 @@ export function subscribeCommandCenterRealtime(onChange: () => void) {
     .on("postgres_changes", { event: "*", schema: "public", table: "dispatch_missions" }, onChange)
     .on("postgres_changes", { event: "*", schema: "public", table: "mission_timeline_events" }, onChange)
     .on("postgres_changes", { event: "*", schema: "public", table: "mission_evidence" }, onChange)
+    .on("postgres_changes", { event: "*", schema: "public", table: "mission_messages" }, onChange)
     .on("postgres_changes", { event: "*", schema: "public", table: "command_center_notifications" }, onChange)
     .subscribe();
 
